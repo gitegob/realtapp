34e4e9f4ef2b11f0ebd0b1d58b3a616e179f1841
Mistico Clement <clementmistico@gmail.com>
2021-03-24 21:58:24 +0200

    bug():fix image upload

--- src/env.ts
+++ src/env.ts
@@ -12,6 +12,9 @@ const {
   TEST_DB_NAME,
   JWT_SECRET,
   JWT_EXPIRES_IN,
+  CLOUD_API_KEY,
+  CLOUD_API_SECRET,
+  CLOUD_NAME,
 } = process.env;
 
 export default {
@@ -24,4 +27,7 @@ export default {
   TEST_DB_NAME,
   JWT_SECRET,
   JWT_EXPIRES_IN,
+  CLOUD_API_SECRET,
+  CLOUD_API_KEY,
+  CLOUD_NAME,
 };
--- src/house/dto/create-house.dto.ts
+++ src/house/dto/create-house.dto.ts
@@ -1,47 +1,24 @@
 import { IsNotEmpty, MinLength } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateHouseDto {
   @IsNotEmpty()
   @MinLength(5, {
     message: 'Title should have more than 5 characters',
   })
-  @ApiProperty({
-    description: 'Title of the post',
-    default: 'Beautiful house',
-  })
   readonly title: string;
 
   @IsNotEmpty()
   @MinLength(10, { message: 'Description should have more than 10 characters' })
-  @ApiProperty({
-    description: 'Description of the post',
-    default: "This is beautiful house, don't miss it",
-  })
   readonly description: string;
 
   @IsNotEmpty()
-  @ApiProperty({
-    description: 'District',
-    default: 'KICUKIRO',
-  })
   readonly district: string;
 
   @IsNotEmpty()
-  @ApiProperty({
-    description: 'Street',
-    default: 'KK 440',
-  })
   readonly street: string;
 
   pictures: string[];
 
   @IsNotEmpty()
-  @ApiProperty({
-    description: 'Price of the house',
-    default: '20000',
-  })
   readonly price: number;
-
-  createdBy: string;
 }
